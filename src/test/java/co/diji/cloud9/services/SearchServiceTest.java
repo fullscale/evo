6561dbad6dec5ab475ae636a4a8a903a055a909a
Matt Weber <matt@mattweber.org>
2012-03-21 14:59:14 -0700

    Initial support for tests.

---
+++ src/test/java/co/diji/cloud9/services/SearchServiceTest.java
@@ -0,0 +1,40 @@
+package co.diji.cloud9.services;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+
+public class SearchServiceTest {
+
+    private SearchService searchService;
+
+    @Before
+    public void setup() {
+        searchService = new SearchService();
+
+        // mock appplication context and inject into search service
+        MockServletContext servletContext = new MockServletContext();
+        WebApplicationContext webappContext = new GenericWebApplicationContext(servletContext);
+        ReflectionTestUtils.setField(searchService, "applicationContext", webappContext, WebApplicationContext.class);
+
+        searchService.booststrap();
+    }
+
+    @After
+    public void shutdown() {
+        if (searchService != null) {
+            searchService.shutdown();
+        }
+    }
+
+    @Test
+    public void testSearchService() {
+        Assert.assertNotNull(searchService);
+    }
+}
