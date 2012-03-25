1dc2bf73e7737d56f520e659f51e1e8a3716b76d
Eric Gaumer <egaumer@gmail.com>
2012-03-24 21:14:04 -0400

    wire up users view

--- src/main/java/co/diji/cloud9/controllers/UserController.java
+++ src/main/java/co/diji/cloud9/controllers/UserController.java
@@ -1,18 +1,33 @@
 package co.diji.cloud9.controllers;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import org.elasticsearch.action.admin.cluster.node.stats.NodeStats;
+import org.elasticsearch.action.admin.cluster.node.info.NodeInfo;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.elasticsearch.cluster.ClusterState;
+
+import co.diji.cloud9.services.SearchService;
 
 @Controller
 @RequestMapping("/cloud9/user")
 public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    protected SearchService searchService;
 
     @ResponseBody
     @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
@@ -40,8 +55,21 @@ public class UserController {
 
     @ResponseBody
     @RequestMapping(method = RequestMethod.GET)
-    public void list() {
-        logger.info("user list");
+    public ModelAndView list() {
+
+        ClusterHealthResponse clusterHealth = searchService.getClusterHealth();
+        //Map<String, Integer> clusterStatus = searchService.getClusterStatus();
+        Map<String, NodeInfo> nodeInfo = searchService.getNodeInfo();
+        Map<String, NodeStats> nodeStats = searchService.getNodeStats();
+
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("users");
+        mav.addObject("cluster", clusterHealth);
+        mav.addObject("stats", nodeStats);
+        mav.addObject("nodes", nodeInfo);
+        //mav.addObject("status", clusterStatus);
+        //mav.addObject("build", "build" + app.build);
+        return mav;
     }
 
 }
