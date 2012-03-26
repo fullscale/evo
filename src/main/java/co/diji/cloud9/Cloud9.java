cc678ea5cfe09cbc5ce419c7d58e0862c8ca88e5
Eric Gaumer <egaumer@gmail.com>
2012-03-26 16:18:04 -0400

    add SSL support

--- src/main/java/co/diji/cloud9/Cloud9.java
+++ src/main/java/co/diji/cloud9/Cloud9.java
@@ -1,24 +1,158 @@
 package co.diji.cloud9;
 
+import java.io.File;
 import java.net.URL;
 import java.security.ProtectionDomain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.server.ssl.SslSocketConnector;
+
+import org.fuin.utils4j.Utils4J;
 
 public final class Cloud9 {
-    public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
-        ProtectionDomain domain = Cloud9.class.getProtectionDomain();
-        URL location = domain.getCodeSource().getLocation();
 
-        WebAppContext webapp = new WebAppContext();
-        webapp.setContextPath("/");
-        webapp.setWar(location.toExternalForm());
+	private static final Logger logger = LoggerFactory.getLogger(Cloud9.class);
 
-        server.setStopAtShutdown(true);
-        server.setHandler(webapp);
-        server.start();
-        server.join();
-    }
+	public static void main(String[] args) throws Exception {
+
+		// read user specified properties
+		int httpPort = Integer.parseInt(
+            System.getProperty("c9.http.port", "2600")
+        );
+
+		int httpsPort = Integer.parseInt(
+            System.getProperty("c9.http.port", "2643")
+        );
+
+		Boolean enableHttps = "true".equals(
+            System.getProperty("c9.https.enable", "false")
+        );
+
+		Boolean forceHttps = "force".equals(
+            System.getProperty("c9.https.enable", "false")
+        );
+        
+		String libdir = System.getProperty("c9.libdir", null);
+
+		Server server = new Server(httpPort);
+
+		ProtectionDomain domain = Cloud9.class.getProtectionDomain();
+		URL location = domain.getCodeSource().getLocation();
+
+		// load external jars
+		if (libdir == null) {
+			// checks default location
+			File base = new File(location.getPath());
+			libdir = base.getParent() + "/lib";
+		}
+		loadExternalLibs(libdir);
+
+		WebAppContext webapp = new WebAppContext();
+
+		// create a temporary directory to run from
+		File tmpDir = new File(System.getProperty("java.io.tmpdir") + "cloud9-"
+				+ httpPort);
+
+		// if it already exists delete it
+		if (tmpDir.exists()) {
+			deleteDirectory(tmpDir);
+		}
+
+		// create a new empty directory
+		if (!(tmpDir.mkdir())) {
+			throw new RuntimeException("Unable to create temporary dir: "
+					+ tmpDir);
+		}
+
+		// this depends on how the JVM was shutdown
+		tmpDir.deleteOnExit();
+		logger.debug("temp dir: {}", tmpDir);
+
+		// setup an SSL socket if user requested one
+		if (enableHttps || forceHttps) {
+
+			// get the keystore/keypass otherwise use cloud9 default
+			String keypass = System.getProperty("c9.https.keypass",
+					"3f038de0-6606-11e1-b86c-0800200c9a66");
+			String keystore = System.getProperty("c9.https.keystore", tmpDir
+					+ "/webapp/WEB-INF/security/c9.default.keystore");
+
+			// create a secure channel
+			final SslContextFactory sslContextFactory = new SslContextFactory(
+					keystore);
+			sslContextFactory.setKeyStorePassword(keypass);
+			sslContextFactory.setKeyManagerPassword(keypass);
+
+			final SslSocketConnector sslConn = new SslSocketConnector(
+					sslContextFactory);
+			sslConn.setPort(httpsPort);
+
+			// we're not forcing HTTPS so start an HTTP channel as well
+			if (!forceHttps) {
+				logger.info("Enabling SSL on port {}", httpsPort);
+				SelectChannelConnector selectChannelConnector = new SelectChannelConnector();
+				selectChannelConnector.setPort(httpPort);
+				server.setConnectors(new Connector[] { sslConn,
+						selectChannelConnector });
+			} else {
+				logger.info("Enforcing SSL on port {}", httpsPort);
+				server.setConnectors(new Connector[] { sslConn });
+			}
+		}
+
+		webapp.setContextPath("/");
+		webapp.setTempDirectory(tmpDir);
+		webapp.setWar(location.toExternalForm());
+
+		server.setStopAtShutdown(true);
+		server.setHandler(webapp);
+		server.start();
+		server.join();
+
+	}
+
+	/*
+	 * Loads any external dependencies.
+	 */
+	private static void loadExternalLibs(String directory) {
+		try {
+			File dir = new File(directory);
+			String[] files = dir.list();
+
+			if (files != null) {
+				logger.info("Loading external dependencies: {}", directory);
+				for (int i = 0; i < files.length; i++) {
+					String fileName = "file:" + directory + "/" + files[i];
+					Utils4J.addToClasspath(fileName);
+				}
+			}
+		} catch (Exception e) {
+			logger.warn("Error loading external JAR files");
+		}
+	}
+
+	/*
+	 * Really? Java has no way of deleting non-empty directories.
+	 */
+	public static boolean deleteDirectory(File path) {
+		if (path.exists()) {
+			File[] files = path.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].isDirectory()) {
+					deleteDirectory(files[i]);
+				} else {
+					files[i].delete();
+				}
+			}
+		}
+		return (path.delete());
+	}
+
 }
