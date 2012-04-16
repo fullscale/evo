716b4c48fbc8f7376bcb464869c7976cf93dc795
Matt Weber <matt@mattweber.org>
2012-04-16 10:33:46 -0700

    prepare document controller
    update logging, add json response, add search service

--- src/main/java/co/diji/cloud9/controllers/DocumentController.java
+++ src/main/java/co/diji/cloud9/controllers/DocumentController.java
@@ -1,12 +1,18 @@
 package co.diji.cloud9.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import co.diji.cloud9.services.SearchService;
 
 @Controller
 @RequestMapping("/cloud9/content")
@@ -14,28 +20,39 @@ public class DocumentController {
 
     private static final Logger logger = LoggerFactory.getLogger(DocumentController.class);
 
+    @Autowired
+    protected SearchService searchService;
+
     @ResponseBody
-    @RequestMapping(value = "/{collection}/{type}/{id}", method = RequestMethod.GET)
-    public void get(@PathVariable String collection, @PathVariable String type, @PathVariable String id) {
-        logger.info("document get collection:" + collection + " type:" + type + " id:" + id);
+    @RequestMapping(value = "/{collection}/{type}/{id}", method = RequestMethod.GET, produces = "application/json")
+    public Map<String, Object> get(@PathVariable String collection, @PathVariable String type, @PathVariable String id) {
+        logger.trace("in controller=document action=get collection:{} type:{} id:{}", new Object[]{collection, type, id});
+        Map<String, Object> resp = new HashMap<String, Object>();
+        return resp;
     }
 
     @ResponseBody
-    @RequestMapping(value = "/{collection}/{type}/{id}", method = RequestMethod.POST)
-    public void create(@PathVariable String collection, @PathVariable String type, @PathVariable String id) {
-        logger.info("document create collection:" + collection + " type:" + type + " id:" + id);
+    @RequestMapping(value = "/{collection}/{type}/{id}", method = RequestMethod.POST, produces = "application/json")
+    public Map<String, Object> create(@PathVariable String collection, @PathVariable String type, @PathVariable String id) {
+        logger.trace("in controller=document action=create collection:{} type:{} id:{}", new Object[]{collection, type, id});
+        Map<String, Object> resp = new HashMap<String, Object>();
+        return resp;
     }
 
     @ResponseBody
-    @RequestMapping(value = "/{collection}/{type}/{id}", method = RequestMethod.PUT)
-    public void update(@PathVariable String collection, @PathVariable String type, @PathVariable String id) {
-        logger.info("document update collection:" + collection + " type:" + type + " id:" + id);
+    @RequestMapping(value = "/{collection}/{type}/{id}", method = RequestMethod.PUT, produces = "application/json")
+    public Map<String, Object> update(@PathVariable String collection, @PathVariable String type, @PathVariable String id) {
+        logger.trace("in controller=document action=update collection:{} type:{} id:{}", new Object[]{collection, type, id});
+        Map<String, Object> resp = new HashMap<String, Object>();
+        return resp;
     }
 
     @ResponseBody
-    @RequestMapping(value = "/{collection}/{type}/{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable String collection, @PathVariable String type, @PathVariable String id) {
-        logger.info("document delete collection:" + collection + " type:" + type + " id:" + id);
+    @RequestMapping(value = "/{collection}/{type}/{id}", method = RequestMethod.DELETE, produces = "application/json")
+    public Map<String, Object> delete(@PathVariable String collection, @PathVariable String type, @PathVariable String id) {
+        logger.trace("in controller=document action=delete collection:{} type:{} id:{}", new Object[]{collection, type, id});
+        Map<String, Object> resp = new HashMap<String, Object>();
+        return resp;
     }
 
 }
