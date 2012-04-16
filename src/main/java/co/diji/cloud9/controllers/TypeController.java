d3c8a94762ddaf22bde22a56ceab59fdf4d0ed57
Matt Weber <matt@mattweber.org>
2012-04-16 09:06:22 -0700

    types controller update action

--- src/main/java/co/diji/cloud9/controllers/TypeController.java
+++ src/main/java/co/diji/cloud9/controllers/TypeController.java
@@ -3,13 +3,16 @@ package co.diji.cloud9.controllers;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.elasticsearch.cluster.metadata.MappingMetaData;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -66,9 +69,47 @@ public class TypeController {
     }
 
     @ResponseBody
-    @RequestMapping(value = "/{collection}/{type}", method = RequestMethod.PUT)
-    public void update(@PathVariable String collection, @PathVariable String type) {
-        logger.trace("in controller=type action=update collection:{} type:{}", collection, type);
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value = "/{collection}/{type}", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
+    public Map<String, Object> update(@PathVariable String collection, @PathVariable String type,
+            @RequestBody Map<String, Object> mappings) {
+        logger.trace("in controller=type action=update collection:{} type:{} mappings:{}", new Object[]{collection, type, mappings});
+        Map<String, Object> resp = new HashMap<String, Object>();
+
+        try {
+            MappingMetaData typeInfo = searchService.getType(collection, type);
+            logger.debug("typeInfo: {}", typeInfo);
+            if (typeInfo == null) {
+                throw new Cloud9Exception("Unable to get type info for: " + type);
+            }
+
+            Map<String, Object> currentMappings = typeInfo.sourceAsMap();
+            Map<String, Object> currentFields = (Map<String, Object>) currentMappings.get("properties");
+            logger.debug("currentMappings: {}", currentMappings);
+            logger.debug("currentFields: {}", currentFields);
+            for (Entry<String, Object> mapping : mappings.entrySet()) {
+                String fieldName = mapping.getKey();
+                logger.debug("fieldName: {}", fieldName);
+                if (currentFields.containsKey(fieldName)) {
+                    throw new Cloud9Exception("Field already exists: " + fieldName);
+                }
+
+                currentFields.put(fieldName, mapping.getValue());
+            }
+
+            JSONObject newMapping = new JSONObject();
+            newMapping.put(type, currentMappings);
+            logger.debug("newMapping: {}", newMapping);
+            searchService.putMapping(collection, type, newMapping.toJSONString());
+
+            resp.put("status", "ok");
+        } catch (Exception e) {
+            logger.warn("Error updating mappings: {}", e.getMessage());
+            resp.put("status", "error");
+            resp.put("response", e.getMessage());
+        }
+
+        return resp;
     }
 
     @ResponseBody
