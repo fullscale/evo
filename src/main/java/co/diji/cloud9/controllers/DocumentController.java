99aa666eb64fbe2dd77a0558fb57afc9e3b3d13c
Matt Weber <matt@mattweber.org>
2012-04-16 12:35:51 -0700

    document controller delete action

--- src/main/java/co/diji/cloud9/controllers/DocumentController.java
+++ src/main/java/co/diji/cloud9/controllers/DocumentController.java
@@ -3,6 +3,7 @@ package co.diji.cloud9.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import co.diji.cloud9.exceptions.Cloud9Exception;
 import co.diji.cloud9.services.SearchService;
 
 @Controller
@@ -52,6 +54,23 @@ public class DocumentController {
     public Map<String, Object> delete(@PathVariable String collection, @PathVariable String type, @PathVariable String id) {
         logger.trace("in controller=document action=delete collection:{} type:{} id:{}", new Object[]{collection, type, id});
         Map<String, Object> resp = new HashMap<String, Object>();
+
+        try {
+            DeleteResponse delResp = searchService.deleteDoc(collection, type, id);
+            logger.debug("delResp: {}", delResp);
+            if (delResp == null) {
+                throw new Cloud9Exception("Error deleteing document: " + id);
+            }
+
+            resp.put("status", "ok");
+            resp.put("id", delResp.id());
+            resp.put("version", delResp.version());
+        } catch (Cloud9Exception e) {
+            logger.warn(e.getMessage());
+            resp.put("status", "error");
+            resp.put("response", e.getMessage());
+        }
+
         return resp;
     }
 
