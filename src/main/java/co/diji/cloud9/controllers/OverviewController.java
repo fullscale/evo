811927da76f7527c9ca569eed6a4da8c5f90435c
Eric Gaumer <egaumer@gmail.com>
2012-05-17 10:06:43 -0700

    Controllers: setup redirect for application root
    Causes requests to "/" and "/cloud9" to properly redirect to cluster
    overview page.

--- src/main/java/co/diji/cloud9/controllers/OverviewController.java
+++ src/main/java/co/diji/cloud9/controllers/OverviewController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class OverviewController extends BaseController {
@@ -15,7 +16,15 @@ public class OverviewController extends BaseController {
     private static final Logger logger = LoggerFactory.getLogger(OverviewController.class);
 
     @ResponseBody
-    @RequestMapping(value = {"/", "/cloud9", "/cloud9/overview"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/", "/cloud9"}, method = RequestMethod.GET)
+    public ModelAndView getIndex(ModelMap model) {
+        RedirectView redirect = new RedirectView("/cloud9/overview");
+        redirect.setExposeModelAttributes(false);
+        return new ModelAndView(redirect);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = {"/cloud9/overview"}, method = RequestMethod.GET)
     public ModelAndView get(ModelMap model) {
         logger.trace("enter controller=overview action=get");
         return new ModelAndView("overview", model);
