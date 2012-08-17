9228222be6f9bda8ff5ec393fe30768d4324751f
Matt Weber <matt@mattweber.org>
2012-08-16 17:10:10 -0700

    Configure hazelcast from settings in configService
    Use the ConfigService to read settings vs reading directly from the
    elastic search settings object.

--- src/main/java/co/diji/cloud9/services/HazelcastService.java
+++ src/main/java/co/diji/cloud9/services/HazelcastService.java
@@ -9,7 +9,6 @@ import com.hazelcast.config.TcpIpConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
-import org.elasticsearch.common.settings.Settings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,24 +30,23 @@ public class HazelcastService {
 
         // read default cloud9 hazelcast settings
         Config conf = new ClasspathXmlConfig("hazelcast-cloud9.xml");
-        Settings nodeSettings = configService.getNodeSettings();
 
         // set our hazelcast instance name to cloud9 so we can configure the
         // session caching to use the same instance
         conf.setInstanceName("cloud9");
 
         // hazelcast group name should be the same as our elasticsearch cluster name
-        conf.getGroupConfig().setName(nodeSettings.get("cluster.name"));
+        conf.getGroupConfig().setName(configService.get("cluster.name"));
 
-        // if elasticsearch is in unicast mode, use the same settings for hazelcast
-        boolean useMulticast = nodeSettings.getAsBoolean("discovery.zen.ping.multicast.enabled", true);
-        logger.debug("useMulticast: {}", useMulticast);
-        if (!useMulticast) {
+        // if unicast is enabled, do the same for hazelcast
+        boolean useUnicast = configService.getBool("network.unicast.enabled", false);
+        logger.debug("useUnicast: {}", useUnicast);
+        if (useUnicast) {
             logger.debug("disabling multicast");
             conf.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
             logger.debug("enabling tcp/ip (unicast)");
             TcpIpConfig tcp = conf.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true);
-            for (String host : nodeSettings.getAsArray("discovery.zen.ping.unicast.hosts")) {
+            for (String host : configService.getArray("discovery.zen.ping.unicast.hosts", new String[]{})) {
                 logger.debug("adding member: {}", host);
                 tcp.addMember(host);
             }
