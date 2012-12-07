a089b75e5387e4406fcc274caea6050eedbc1ba5
Eric Gaumer <egaumer@gmail.com>
2012-12-06 19:14:09 -0500

    call resize when active editor tab changes
    This fixes an annoying bug that causes the editor text to go
    'invisible' when the browser window is resized. Looks like this gets
    called properly for the 'active' tab but not for other tabs. This patch
    ensures resize is called whenever a new tab becomes the active tab.

--- resources/js/c9/ide/ide.js
+++ resources/js/c9/ide/ide.js
@@ -1,8 +1,12 @@
 C9.ide.init = function(app) { 
-  C9.app.name = app
+  C9.app.name = app;
   C9.ide.layout.init();
   C9.ide.navigator.init(app);
   tabView = new YAHOO.widget.TabView('c9-ide-editor');
   Editors = new Object;
   c9 = new cloud9();
+  tabView.subscribe('activeTabChange', function(ev){
+	  var tabId = ev.newValue.get('postData');
+	  Editors[tabId].resize();
+  });
 };
